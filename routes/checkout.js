a294f15acb8eb039871e506a27188163a9a9aae5
Sunny <hitse@paypal.com>
2020-12-15 14:35:15 +0800

    test

--- routes/checkout.js
+++ routes/checkout.js
@@ -13,23 +13,25 @@ router.post('/', function(req, res, next) {
 
   // Use the payment method nonce here
   var nonceFromTheClient = req.body.paymentMethodNonce;
+
+  console.log("nonce")
+  console.log(req.body.paymentMethodNonce)
   // Create a new transaction for $10
-  // var newTransaction = gateway.transaction.sale({
-  //   amount: '22.00',
-  //   paymentMethodNonce: nonceFromTheClient,
-  //   options: {
-  //     // This option requests the funds from the transaction
-  //     // once it has been authorized successfully
-  //     submitForSettlement: true
-  //   }
-  // }, function(error, result) {
-  //     if (result) {
-  //       res.send(result);
-  //     } else {
-  //       res.status(500).send(error);
-  //     }
-  // });
-  res.send({paymentMethodNonce: req.body.paymentMethodNonce})
+  var newTransaction = gateway.transaction.sale({
+    amount: '22.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
 });
 
 module.exports = router;
