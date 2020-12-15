9330c94bfcc6aadb7458f03db68eab14ca8a07ff
Sunny <hitse@paypal.com>
2020-12-15 12:11:45 +0800

    auth api fix

--- routes/getauth.js
+++ routes/getauth.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var braintree = require('braintree');
 
-router.post('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   var gateway = braintree.connect({
     environment: braintree.Environment.Sandbox,
     // Use your own credentials from the sandbox Control Panel here
